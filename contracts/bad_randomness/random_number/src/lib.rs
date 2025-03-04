b2cc3708693483703237a9f78f040ca3776d36a8
Kien Le <kienle371999@gmail.com>
2025-03-04 16:33:08 +0700

    Add contracts containing the vulnerability - bad randomness

---
+++ contracts/bad_randomness/random_number/src/lib.rs
@@ -0,0 +1,34 @@
+use solana_program::{
+    msg,
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    sysvar::{clock::Clock, Sysvar}
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let user_account = next_account_info(accounts_iter)?;
+
+
+    if !user_account.is_signer {
+        msg!("User account must sign the transaction");
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    let clock = Clock::get()?;
+    let block_timestamp = clock.unix_timestamp;
+    let block_number = clock.slot as i64;
+    let random_number = block_timestamp * (block_number + 100);
+
+    msg!("Current block (slot) number: {}", random_number);
+    Ok(())
+}
